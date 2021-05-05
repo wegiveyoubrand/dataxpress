8e3a2e65cb2305f7f14b83e24b6efabb667a8d59
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-05-05 19:29:50 +0100

    Updated.
    1. Scroll to top feature
    2. Reach Us Button
    3. Explore and services Submenus

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const menuToggle = document.querySelector(".menu-toggle");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 
+const btnScrollToTop = document.querySelector(".scroll-to-top");
+
+btnScrollToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
 menuToggle.addEventListener("click", (e) => {
   nav.classList.toggle("active");
   header.classList.toggle("fixed");
